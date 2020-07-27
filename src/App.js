743849a201cead3ab09ae1dc51eeb1f145e97ca4
Matheus <matheusbrbs@gmail.com>
2020-07-27 19:41:57 -0300

    feat: styled-components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { ThemeProvider } from 'styled-components';
+
+import MyTeams from './pages/MyTeams/MyTeams';
+import GlobalStyle from './styles/global';
+import light from './styles/themes/light';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={light}>
+      <MyTeams />
+      <GlobalStyle />
+    </ThemeProvider>
   );
 }
 
---
+++ src/pages/MyTeams/MyTeams.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './MyTeamsStyles';
+
+function MyTeams() {
+  return (
+    <Container>
+      <h1>Helo World !</h1>
+    </Container>
+  );
+}
+
+export default MyTeams;
