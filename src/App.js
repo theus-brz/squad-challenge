52928c8d2cb3e4ade3915e50a96caea6ca79d80f
Matheus <matheusbrbs@gmail.com>
2020-07-27 20:14:56 -0300

    feat: react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 import { ThemeProvider } from 'styled-components';
 
-import MyTeams from './pages/MyTeams/MyTeams';
+import Routes from './routes';
 import GlobalStyle from './styles/global';
 import light from './styles/themes/light';
 
 function App() {
   return (
     <ThemeProvider theme={light}>
-      <MyTeams />
+      <Routes />
       <GlobalStyle />
     </ThemeProvider>
   );
