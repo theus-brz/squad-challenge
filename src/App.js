219721daec54de290df8024d04ae32f33c4babf0
Matheus Barbosa <matheusbrbs@gmail.com>
2020-07-27 21:09:43 -0300

    feat: redux structure, reactotron, eslint small fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
 import { ThemeProvider } from 'styled-components';
 
 import Routes from './routes';
+import './config/reactotronConfig';
+import { store } from './store';
 import GlobalStyle from './styles/global';
 import light from './styles/themes/light';
 
 function App() {
   return (
-    <ThemeProvider theme={light}>
-      <Routes />
-      <GlobalStyle />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={light}>
+        <Routes />
+        <GlobalStyle />
+      </ThemeProvider>
+    </Provider>
   );
 }
 
