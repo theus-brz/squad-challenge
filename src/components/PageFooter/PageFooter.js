c371f071e18ec0d76d30cc9de3ed3d526eb0ec7b
Matheus <matheusbrbs@gmail.com>
2020-07-29 10:22:01 -0300

    feat: pagefooter component

---
+++ src/components/PageFooter/PageFooter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { PageFooterContainer } from './PageFooterStyles';
+
+function PageFooter() {
+  return (
+    <PageFooterContainer>
+      <p>2020 - All rights reserved</p>
+    </PageFooterContainer>
+  );
+}
+
+export default PageFooter;
