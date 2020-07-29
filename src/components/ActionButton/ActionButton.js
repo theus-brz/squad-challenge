6baec8a55e2970f8dadf5baf0bff88e51c2967c1
Matheus <matheusbrbs@gmail.com>
2020-07-29 10:22:46 -0300

    feat: actionbutton component

---
+++ src/components/ActionButton/ActionButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { ActionButtonContainer } from './ActionButtonStyles';
+
+function ActionButton() {
+  return <ActionButtonContainer />;
+}
+
+export default ActionButton;
