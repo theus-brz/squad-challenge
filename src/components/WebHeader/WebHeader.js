9484502946f9bbc4b6584ee31e3f340c2da097d3
Matheus <matheusbrbs@gmail.com>
2020-07-29 10:23:31 -0300

    feat: webheader component

---
+++ src/components/WebHeader/WebHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { WebHeaderContainer } from './WebHeaderStyles';
+
+function WebHeader() {
+  return (
+    <WebHeaderContainer>
+      <p>Squad management tool</p>
+    </WebHeaderContainer>
+  );
+}
+
+export default WebHeader;
---
+++ src/components/WebHeader/WebHeaderStyles.js
@@ -0,0 +1,3 @@
+import styled from 'styled-components';
+
+export const WebHeaderContainer = styled.header``;
